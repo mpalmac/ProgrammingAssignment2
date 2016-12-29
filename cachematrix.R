30e7983fa239a7438300b6777c03143f673a6772
mpalmac <mpalmac@fen.uchile.cl>
2016-12-29 15:51:23 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Make Caché matrix calculates the inverse of a matriz and stores it into the current directory.
+## it might be usefull because inverting a matrix can take time if the matrix is too big. 
+## Both MakeCacheMatrix and cacheSolve are not used directly as function(matrix). The results of each step must be stored so an error does not appear.
+## If cacheSolve is not used before looking makeCacheMatrix, the answer will be NULL: The inverted matrix does not exists. 
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+## Cache solve "looks"if the matrix has been created already. If it is not, it creates it. If the matrix already exists, it does not calculate
+## it again, but instead it "recalls"the existant inverted matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
